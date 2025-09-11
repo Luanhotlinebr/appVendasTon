a8e28020fdc4d75eff96677c5fc845134f5df1d0
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-09-11 14:28:38 -0300

    feat:add pages login,reset-password and home

---
+++ src/assets/Vite + React + TS_files/main.tsx
@@ -0,0 +1,14 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=dd0c63dd"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=dd0c63dd"; const createRoot = __vite__cjsImport1_reactDom_client["createRoot"];
+import "/src/index.css?t=1757598631717";
+import { RouterProvider } from "/node_modules/.vite/deps/react-router-dom.js?v=dd0c63dd";
+import { router } from "/src/routes.tsx?t=1757598631717";
+createRoot(document.getElementById("root")).render(
+  /* @__PURE__ */ jsxDEV(RouterProvider, { router }, void 0, false, {
+    fileName: "C:/Users/Luan Sousa/Documents/.Programacao/Projetos/appVendasTon/src/main.tsx",
+    lineNumber: 8,
+    columnNumber: 3
+  }, this)
+);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJtYXBwaW5ncyI6IkFBT0U7QUFQRixTQUFTQSxrQkFBa0I7QUFDM0IsT0FBTztBQUVQLFNBQVNDLHNCQUFzQjtBQUMvQixTQUFTQyxjQUFjO0FBRXZCRixXQUFXRyxTQUFTQyxlQUFlLE1BQU0sQ0FBRSxFQUFFQztBQUFBQSxFQUMzQyx1QkFBQyxrQkFBZSxVQUFoQjtBQUFBO0FBQUE7QUFBQTtBQUFBLFNBQStCO0FBQ2pDIiwibmFtZXMiOlsiY3JlYXRlUm9vdCIsIlJvdXRlclByb3ZpZGVyIiwicm91dGVyIiwiZG9jdW1lbnQiLCJnZXRFbGVtZW50QnlJZCIsInJlbmRlciJdLCJpZ25vcmVMaXN0IjpbXSwic291cmNlcyI6WyJtYWluLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBjcmVhdGVSb290IH0gZnJvbSBcInJlYWN0LWRvbS9jbGllbnRcIjtcclxuaW1wb3J0IFwiLi9pbmRleC5jc3NcIjtcclxuXHJcbmltcG9ydCB7IFJvdXRlclByb3ZpZGVyIH0gZnJvbSBcInJlYWN0LXJvdXRlci1kb21cIjtcclxuaW1wb3J0IHsgcm91dGVyIH0gZnJvbSBcIi4vcm91dGVzXCI7XHJcblxyXG5jcmVhdGVSb290KGRvY3VtZW50LmdldEVsZW1lbnRCeUlkKFwicm9vdFwiKSEpLnJlbmRlcihcclxuICA8Um91dGVyUHJvdmlkZXIgcm91dGVyPXtyb3V0ZXJ9IC8+XHJcbik7XHJcbiJdLCJmaWxlIjoiQzovVXNlcnMvTHVhbiBTb3VzYS9Eb2N1bWVudHMvLlByb2dyYW1hY2FvL1Byb2pldG9zL2FwcFZlbmRhc1Rvbi9zcmMvbWFpbi50c3gifQ==
