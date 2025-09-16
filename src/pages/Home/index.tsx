76d75c6ec6a09efd90ea0a3ae800769fd2229aac
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-09-16 17:04:59 -0300

    feat:add logout button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,12 @@
+import { useAuth } from "@/contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
 export function Home() {
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  function handleLogout() {
+    logout(navigate);
+  }
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#f2f4f7]">
       <div className="w-full max-w-2xl flex flex-col justify-center items-center p-6 rounded-md shadow-md bg-white">
@@ -11,6 +19,12 @@ export function Home() {
             <label>Valor R$</label>
             <label>R$ 1200,00</label>
           </div>
+          <button
+            className="bg-[#30a66d] w-full p-3 rounded-md text-white mt-6 cursor-pointer"
+            onClick={handleLogout}
+          >
+            Sair
+          </button>
         </div>
       </div>
     </div>
