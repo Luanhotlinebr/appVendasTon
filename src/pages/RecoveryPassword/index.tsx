f08fb03fc45a0965b619977c2c7c73f363bfec4b
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-09-17 15:56:19 -0300

    feat:update width

--- src/pages/RecoveryPassword/index.tsx
+++ src/pages/RecoveryPassword/index.tsx
@@ -36,7 +36,7 @@ export function ForgotPassword() {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#f2f4f7]">
-      <div className="w-full max-w-2xl flex flex-col justify-center items-center p-6 rounded-md shadow-md bg-white">
+      <div className="w-full max-w-3/4 flex flex-col justify-center items-center p-6 rounded-md shadow-md bg-white">
         <form onSubmit={handleSubmit} className="w-full">
           <div className="w-full flex flex-col mt-2 gap-2">
             <h1 className="text-3xl my-2">Recuperar a senha</h1>
