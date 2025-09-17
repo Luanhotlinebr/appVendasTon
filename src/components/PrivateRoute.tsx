2ed7d7af3b82af9fda18ca771a8b916aca2f1a5d
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-09-17 10:06:50 -0300

    feat:update PrivateRoute component

--- src/components/PrivateRoute.tsx
+++ src/components/PrivateRoute.tsx
@@ -1,20 +1,41 @@
 import { Navigate } from "react-router-dom";
 import type { ReactNode } from "react";
+import Icon from "@mdi/react";
+import { mdiAccount } from "@mdi/js";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/contexts/AuthContext";
+import { mdiLogout } from "@mdi/js";
 
 interface PrivateRouteProps {
   children: ReactNode;
 }
 
 export function PrivateRoute({ children }: PrivateRouteProps) {
-  // Aqui você poderia pegar o usuário de um Context ou de um estado global
-  const user = localStorage.getItem("user"); // Exemplo simples
+  const { user, logout } = useAuth();
+
+  const navigate = useNavigate();
+  function handleLogout() {
+    logout(navigate);
+  }
 
   if (!user) {
     return <Navigate to="/login" replace />; // Redireciona para login se não estiver autenticado
   }
   return (
-    <div>
-      <p>Em desenvolvimento</p>
+    <div className="w-full items-center">
+      <div className="flex flex-row space-x-6 p-6 justify-between items-center">
+        <div className="flex flex-row gap-3">
+          <Icon path={mdiAccount} size={1} />
+          <p>Usuário</p>
+        </div>
+
+        <button
+          className="bg-transparent  p-3 rounded-md text-white cursor-pointer"
+          onClick={handleLogout}
+        >
+          <Icon path={mdiLogout} size={1} className="text-black" />
+        </button>
+      </div>
       {children}
     </div>
   );
