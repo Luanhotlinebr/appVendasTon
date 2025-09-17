894a614161b13bb1650683dd18a7681b06547cb2
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-09-17 10:07:36 -0300

    feat: remove button from home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,15 +2,9 @@ import { useAuth } from "@/contexts/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 export function Home() {
-  const navigate = useNavigate();
-  const { logout } = useAuth();
-
-  function handleLogout() {
-    logout(navigate);
-  }
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#f2f4f7]">
-      <div className="w-full max-w-2xl flex flex-col justify-center items-center p-6 rounded-md shadow-md bg-white">
+      <div className="w-full max-w-2xl flex  p-6 rounded-md shadow-md bg-white justify-between">
         <div className="w-full max-w-2xl flex flex-row justify-center items-center p-6 rounded-md shadow-md bg-white">
           <label className="w-full flex flex-col  gap-2">
             Total de vendas realizadas:
@@ -21,12 +15,6 @@ export function Home() {
         </div>
 
         <div className="w-full max-w-2xl flex flex-row justify-center items-center p-6 rounded-md shadow-md bg-white mt-2"></div>
-        <button
-          className="bg-[#30a66d] w-full p-3 rounded-md text-white mt-6 cursor-pointer"
-          onClick={handleLogout}
-        >
-          Sair
-        </button>
       </div>
     </div>
   );
