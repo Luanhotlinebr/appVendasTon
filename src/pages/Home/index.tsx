9f5d6d405609dee23e5ac045f3aedc6fb64d2f67
Luanhotlinebr <luanhotlinebr60@gmail.com>
2025-09-17 10:12:13 -0300

    feat:finishing first phase of Home component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,12 @@
-import { useAuth } from "@/contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
-
 export function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-[#f2f4f7]">
       <div className="w-full max-w-2xl flex  p-6 rounded-md shadow-md bg-white justify-between">
         <div className="w-full max-w-2xl flex flex-row justify-center items-center p-6 rounded-md shadow-md bg-white">
-          <label className="w-full flex flex-col  gap-2">
-            Total de vendas realizadas:
-          </label>
-          <label className="w-full flex flex-col  gap-2">
-            Total de vendas realizadas:
-          </label>
+          <label className="w-full flex flex-col  gap-2">Em progresso</label>
+          <label className="w-full flex flex-col  gap-2">Em progresso</label>
         </div>
-
-        <div className="w-full max-w-2xl flex flex-row justify-center items-center p-6 rounded-md shadow-md bg-white mt-2"></div>
+        <div className="w-full max-w-2xl flex flex-row justify-center items-center p-6 rounded-md shadow-md bg-white"></div>
       </div>
     </div>
   );
